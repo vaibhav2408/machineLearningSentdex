13ed74385b00e37bcd11058ee754441eea17908c
vaibhavrai46 <vaibhav@bidgely.com>
2018-01-21 15:39:36 +0530

    Changed the size of verification data to 10%, added unit test

--- ML4_ForecastingAndPredicting.py
+++ ML4_ForecastingAndPredicting.py
@@ -20,7 +20,7 @@ df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace = True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
@@ -42,7 +42,7 @@ accuracy = clf.score(X_test, y_test)
 
 forecast_set = clf.predict(X_lately)
 
-print(forecast_set, accuracy, forecast_out)
+#print(forecast_set, accuracy, forecast_out)
 
 df['Forecast'] = np.nan
 
@@ -57,10 +57,9 @@ for i in forecast_set:
 	next_unix += one_day
 	df.loc[next_date] = [np.nan for _ in range(len(df.columns) -1)] + [i]
 
-
 df['Adj. Close'].plot()
 df['Forecast'].plot()
 plt.legend(loc=4)
 plt.xlabel('Date')
 plt.ylabel('Price')
-plt.show()
+#plt.show()
--- ML9_TestingAssumptions.py
+++ ML9_TestingAssumptions.py
@@ -2,11 +2,30 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1,2,3,4,5,6], dtype = np.float64)
-ys = np.array([5,4,6,5,6,7], dtype = np.float64)
+#xs = np.array([1,2,3,4,5,6], dtype = np.float64)
+#ys = np.array([5,4,6,5,6,7], dtype = np.float64)
+
+# hm - How many data points we want; variance - how variable the data set should be; step = how far on avg. to step up the y value per point; corelation - Either positive or negetive
+
+def create_dataset(hm, variance, step=2, corelation=False): 
+	val = 1
+	ys = []
+	for i in range(hm):
+		y = val + random.randrange(-variance, variance)
+		ys.append(y)
+		if(corelation and corelation== 'pos'):
+			val+=step
+		elif corelation and corelation== 'neg':
+			val -= step
+
+	xs = [i for i in range(len(ys))]
+
+	return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+
 
 def best_fit_slope_and_intercept(xs, ys):
 	m = (  ((mean(xs)*mean(ys)) - mean(xs*ys)) / 
@@ -26,11 +45,16 @@ def coefficient_of_determination(ys_orig, ys_line):
 	squared_error_y_mean = squared_error(ys_orig, y_mean_line)
 	return 1 - (squared_error_regr/squared_error_y_mean)
 
+
+xs, ys = create_dataset(40, 10, 2, corelation='pos')
+
+
+
 m, b = best_fit_slope_and_intercept(xs, ys)
 
 regression_line = [(m*x)+b for x in xs]
 
-predict_x = 1.5
+predict_x = 33
 predict_y = (m*predict_x + b)
 
 
@@ -38,7 +62,6 @@ r_squared = coefficient_of_determination(ys, regression_line)
 print(r_squared)
 
 plt.scatter(xs, ys)
-plt.scatter(predict_x, predict_y, color='g')
 plt.plot(xs, regression_line)
+plt.scatter(predict_x, predict_y,s=100, color='g')
 plt.show()
-
